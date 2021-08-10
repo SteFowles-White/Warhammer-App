c947c0ee90bb576d21e3361dc7228f04072ade05
SteFowles-White <fowleswhite@gmail.com>
2021-08-10 22:35:27 +0100

    update for the hex map

--- src/Components/Hex/HexContainer.js
+++ src/Components/Hex/HexContainer.js
@@ -1,11 +1,28 @@
 import React from "react";
+import Hex from "./Hex";
+// import HexContainer from './HexContainer';
 
 const HexContainer = (props) => {
+    const HexContainerInformation = props.data;
+    const HexArray = [];
+    const HexContainerClasses = `hex__container hex-row-${props.hex_row}`;
 
-    const HexContainerClasses = `hex__container {props.hex_row}`
+    for (const key in HexContainerInformation) {
+        HexArray.push(HexContainerInformation[key]);
+    }
+    console.log(HexArray);
+
+
+    // console.log(HexContainerInformation);
     
     return (
         <div className={HexContainerClasses}> 
+            {
+                HexArray.map((result, key) => {
+                    return <Hex data={result} key={key}/>
+                })
+            }
+
 
         </div>
     )
