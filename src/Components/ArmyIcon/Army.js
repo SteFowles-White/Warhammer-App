eb022886296b66a3b444e083740eaf260ebec99d
SteFowles-White <fowleswhite@gmail.com>
2021-08-12 23:44:01 +0100

    I lifted the state up from the army to the hex - looking into the use of a customer hook for state managment as I lossing previous data when I move to a different hex component

--- src/Components/ArmyIcon/Army.js
+++ src/Components/ArmyIcon/Army.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import orkImage from '../../assets/images/ork-army.png'
 
-const Army = (props) => {
+const Army = React.memo((props) => {
 
-    
+    const getLocationHandler = () => {
+        props.onClickEvent(props.mapPointX, props.mapPointY);
+    }
+
     return(
-        <div id={props.idName} className="text-center" draggable>
-            <img src={orkImage} alt="Ork Army" className="army__icon"/>
+        <div id={props.idName} className="text-center" draggable onMouseEnter={getLocationHandler}>
+            <img src={orkImage} alt="Ork Army" className="army__icon" x={props.mapPointX}/>
         </div>
     )
-}
+})
 
 export default Army;
--- src/Components/Hex/Hex.js
+++ src/Components/Hex/Hex.js
@@ -1,31 +1,71 @@
 import React from "react";
-import { useState } from "react";
+ import { useState} from "react";
 import OrkArmy from "../ArmyIcon/Army";
 
 const Hex = (props) => {
-    const [armyPresent, setArmyPresent] = useState(false);
+//I think these state andlogic need to be in a custom hook beacuse everytime I 
+// move the army icon to another hex I loose the getArmyCordinares data
+//So when I get back to it I an going to try and make a customer hook with state information to see if 
+//holding state data outside of the hex solves this problem
+
     const data = props.data;
+    const [getArmyCordinates, setgetArmyCordinates] = useState({});
+    let armyPresent = false;
+    // let armyPosition = {};
 
-
-    //on drop use a useEffect to change the data of the hexs in questions
+    if (data.army.isArmy) {
+        armyPresent = true;
+    }
+ 
+    const getLocation = (x, y)=> {
+        setgetArmyCordinates({x: x, y: y});
+      }
+      console.log(getArmyCordinates);
 
     const dropHandler = (e) => {
         e.preventDefault();
-        e.target.appendChild(document.getElementById('ork-1'))
+
+        e.target.appendChild(document.getElementById("ork-1"));
+        const targetX = e.target.getAttribute("x");
+        const targetY = e.target.getAttribute("y");
+
+        console.log("current location", targetX, targetY);
+        // console.log("getArmyLocation", getArmyCordinates)
         //change json data so is army === true and the current hex is army === false
-    }
+    };
+    const dragHandler = (e) => {
+        e.preventDefault();
+        console.log('getArmyCordinates', getArmyCordinates);
+        // console.log(e.target.getAttribute("x"))
+        // setArmyCordinates({x: data.x, y: data.y})
+    };
 
 
-    return (
-        <div className="hex">
-            <div className="hex_top"></div>  
-                <div className="hex_body" id={data.id} onDrop={dropHandler} onDragOver={dropHandler.bind(this)}>
 
-                    {data.army.isArmy && <OrkArmy idName="ork-1"/>}
-                </div>
-            <div className="hex_bottom"></div>
-        </div>
-    )
-}
+
+  return (
+    <div className="hex">
+      <div className="hex_top"></div>
+      <div
+        className="hex_body"
+        id={data.id}
+        onDragOver={dragHandler}
+        onDrop={dropHandler}
+        x={data.x}
+        y={data.y}
+      >
+        {armyPresent && (
+          <OrkArmy
+            idName="ork-1"
+            mapPointX={data.x}
+            mapPointY={data.y}
+            onClickEvent={getLocation}
+          />
+        )}
+      </div>
+      <div className="hex_bottom"></div>
+    </div>
+  );
+};
 
 export default Hex;
